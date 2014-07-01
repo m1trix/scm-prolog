5b405672b62ff1ed27bbd8754c651330692c0048
m1trix <teodor.klimentov@gmail.com>
2014-07-01 17:19:53 +0300

    EDIT: Some experiments.

--- src/logical_programming/core.clj
+++ src/logical_programming/core.clj
@@ -3,7 +3,21 @@
 (ns logical-programming.core
   (:gen-class))
 
+(def stack (atom []))
+
+(defn resolute [[clause & query]]
+  (if (false? clause)
+    (if (empty? @stack)
+	  false
+	  (swap! stack ))
+	(if (empty? query)
+	  true
+      (resolute (vec query)))))
+
+(defmacro ?- [clause & query]
+  `(resolute [~clause ~@query]))
+
 (defn -main
   "Entry Point."
   [& args]
-  (println "Hello, World!"))
+  (println (?- true)))	
