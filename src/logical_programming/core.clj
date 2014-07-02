b1b56ad5e377d9dbec0cc2816dbb440a613ac69e
m1trix <teodor.klimentov@gmail.com>
2014-07-02 13:10:39 +0300

    EDIT: Console user interface added.

--- src/logical_programming/core.clj
+++ src/logical_programming/core.clj
@@ -3,21 +3,15 @@
 (ns logical-programming.core
   (:gen-class))
 
-(def stack (atom []))
-
-(defn resolute [[clause & query]]
-  (if (false? clause)
-    (if (empty? @stack)
-	  false
-	  (swap! stack ))
-	(if (empty? query)
-	  true
-      (resolute (vec query)))))
-
-(defmacro ?- [clause & query]
-  `(resolute [~clause ~@query]))
 
 (defn -main
   "Entry Point."
   [& args]
-  (println (?- true)))	
+  (println "SCM-Prolog started.")
+  (let [input (atom "")]
+    (while (not (= @input "exit"))
+      (do
+        (print "?- ")
+        (reset! input (read-line))
+        ))
+    (println "\nHope to see you again soon!\n")))
