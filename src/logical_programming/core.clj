48ad42965c24904d04062b4ecc3e7af723522335
m1trix <teodor.klimentov@gmail.com>
2014-07-02 14:11:54 +0300

    ADD: Console interface now have colors. It takes advantage of the Terminal color commands.

--- src/logical_programming/core.clj
+++ src/logical_programming/core.clj
@@ -4,14 +4,24 @@
   (:gen-class))
 
 
+(defn parse
+  "Parsing the user input and executing it's meaning."
+  [input]
+  (let [trim-input (clojure.string/trim input)]
+    ))
+
+
 (defn -main
   "Entry Point."
   [& args]
-  (println "SCM-Prolog started.")
+  (println "\u001b[33mWellcome to SCM-Prolog! Have fun :) \u001b[0m \n")
   (let [input (atom "")]
     (while (not (= @input "exit"))
       (do
-        (print "?- ")
+        (print "?- \u001b[33m")
+        (flush)
         (reset! input (read-line))
+        (parse @input)
+        (println "\u001b[0m")
         ))
-    (println "\nHope to see you again soon!\n")))
+    (println "\u001b[33mHope to see you again soon! \u001b[0m \n")))
