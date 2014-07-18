10935c811ba774c856741a6e45f9abdea9a408ee
m1trix <teodor.klimentov@gmail.com>
2014-07-18 11:33:49 +0300

    BRANCH: Parser.

--- src/logic/parser.clj
+++ src/logic/parser.clj
@@ -1,1 +1,8 @@
-(ns logic.parser)
+;; ===========================================================
+;;  The Parser is the link between the user input and the
+;;  Interpreter. The Parser reads a string from the user,
+;;  'understands' it't meaning and creates the PrologFunctors
+;;  that it describes.
+;;
+(ns logic.parser
+  [:use [logic.term]])
